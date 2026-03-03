54d865f91833f1e91ddc4e777011f592305fa93b
Hibat-Allah-RGUITI <h.allahrguiti@void.fr>
2026-03-03 06:36:22 -0800

    Added admin configuration menu links for Hello World Module

--- web/modules/custom/hello_world/src/Controller/HelloController.php
+++ web/modules/custom/hello_world/src/Controller/HelloController.php
@@ -1,13 +1,24 @@
 <?php
 
 namespace Drupal\hello_world\Controller;
+
 use Drupal\Core\Controller\ControllerBase;
 
-class HelloController extends ControllerBase {
-  public function content() {
+class HelloController extends ControllerBase
+{
+  public function content()
+  {
     return [
       '#type' => 'markup',
       '#markup' => $this->t('Hello World'),
+    ];
+  }
+
+  public function systemAdminMenuBlockPage()
+  {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Hello World Admin Page'),
     ];
   }
 }
